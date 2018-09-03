6d3817a6eac678087e70f486d55d4bf26b45c731
nitesh gupta <nitesh.gupta@galaxyweblinks.in>
2018-09-03 17:41:36 +0530

    all

--- modules/trainer/views/trainer_dashboard.php
+++ modules/trainer/views/trainer_dashboard.php
@@ -4,10 +4,11 @@
     </div>
 </div>
 
-<div class="row">
+<div class="row" style="margin-bottom:20px;">
     <div class="col-lg-12">
         <div class="col-lg-4 col-sm-4 m-bottom">
-            <button class="form-control btn btn-success box">Total Yogi</button>
+            <button class="form-control btn btn-success box">Total Yogi <br><label class="control-label" for="role_id">20</label></button>
+
         </div>
 
         <div class="col-lg-4 col-sm-4 m-bottom">
@@ -21,11 +22,11 @@
 </div>    
 <div class="row ">
     <div class="col-lg-12">
-        <div class="col-lg-4 col-sm-4 m-bottom">
+        <div class="col-lg-6 col-sm-4 m-bottom">
             <button class="form-control btn btn-success box">Current Month Monthly<br/> Feedback Given</button>
         </div>
         
-        <div class="col-lg-4 col-sm-4 m-bottom">
+        <div class="col-lg-6 col-sm-4 m-bottom">
             <button class="form-control btn btn-success box">Current Month Monthly<br/> Feedback Not Given</button>
         </div>
     </div>
