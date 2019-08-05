6cae888db3931bed32140e712476b2995d692148
mohd shahrukh <mohd.shahrukh@galaxyweblinks.in>
2019-08-05 15:52:20 +0530

    all

--- ypvApp-App-Dev-Backup/platforms-old/android/assets/www/js/config-urls.js
+++
@@ -1,7 +0,0 @@
-/* Web service urls */
-var _protocol = 'http';
-var wsBasePath = _protocol+"://"+"dev.galaxyweblinks.com/ypv-feedback/";
-//var wsBasePath = _protocol+"://"+"192.168.1.101/ypv-feedback/dev/";
-//var wsBasePath = _protocol+"://"+"localhost/ypv-feedback/dev/";
-var wsModule = "api/";
-var wsBaseUrl = wsBasePath+wsModule;
--- ypvApp-App-Dev-Backup/www-old/js/config-urls.js
+++
@@ -1,6 +0,0 @@
-/* Web service urls */
-var _protocol = 'http';
-var wsBasePath = _protocol+"://"+"dev.galaxyweblinks.com/ypv-feedback/";
-//var wsBasePath = _protocol+"://"+"192.168.1.24/ypv-feedback/dev/";
-var wsModule = "api/";
-var wsBaseUrl = wsBasePath+wsModule;
