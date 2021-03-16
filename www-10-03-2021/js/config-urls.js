7020066deec943725d8e53038bc046f943a6ff71
suneel.patel <suneel.patel@galaxyweblinks.in>
2021-03-16 15:03:56 +0530

    android prod build 10032021

---
+++ www-10-03-2021/js/config-urls.js
@@ -0,0 +1,12 @@
+/* Web service urls */
+var _protocol = 'https';
+var wsBasePath = _protocol+"://"+"ypvhealing.com/feedback/";
+//var wsBasePath = _protocol+"://"+"dev.galaxyweblinks.com/ypv-feedback/";
+// var wsBasePath = _protocol+"://"+"ypv-feedback.galaxyweblinks.in/";
+// var wsBasePath = _protocol+"://"+"192.168.0.60/ypv-feedback/dev/";
+// var wsBasePath = _protocol+"://"+"192.168.0.58/ypv-feedback/dev/";
+// var wsBasePath = _protocol+"://"+"localhost/ypv-feedback/dev/";
+//var wsBasePath = _protocol+"://"+"192.168.8.68:81/ypv-feedback/dev/";
+var wsModule = "api/";
+var currentAppVersion = '1.21'
+var wsBaseUrl = wsBasePath+wsModule;
