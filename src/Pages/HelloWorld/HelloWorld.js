f0add3b203d00baa1f8f91dd7060b275d402b923
Ashu <ashutosh.s@reversethought.com>
2022-01-28 12:25:50 +0530

    debugged conflict error in hello world ann share index page

--- src/Pages/HelloWorld/HelloWorld.js
+++ src/Pages/HelloWorld/HelloWorld.js
@@ -1,21 +1,7 @@
 import React from 'react'
 import './HelloWorld.css'
 
-<<<<<<< HEAD
-import {Text} from '../../components/shared';
-
-const HelloWorld = (props) => {
-  return (
-    <div className="App">
-      
-       {/* <Label>Hello</Label> */}
-       {/* <Button /> */}
-       <Text variant="smText"  color="primaryColor" />
-       <Text variant="mdText" color="primaryColor"/>
-       <Text variant="lgText" color="primaryColor"/>
-      
-=======
-import {Heading,SelectItem,SelectGroup,Input,Button,Label} from '../../components/shared';
+import {Heading,SelectItem,SelectGroup,Input,Button,Label,Text} from '../../components/shared';
 const HelloWorld = (props) => {
   return (
     <div className="App">
@@ -51,9 +37,12 @@ const HelloWorld = (props) => {
           <SelectItem>1</SelectItem>
           <SelectItem>2</SelectItem>
         </SelectGroup>
->>>>>>> 3c5a769bab16e63172b8d89247e89221dc2d1895
+
+        <Text variant="smText"  color="primaryColor" />
+       <Text variant="mdText" color="SecondaryColor"/>
+       <Text variant="lgText" color="blackColor"/>
     </div>
   );
 }
 
-export default HelloWorld
+export default HelloWorld;
